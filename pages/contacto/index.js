d12b62273d6cb0d0bcfe7d1e33c18df5d7a04eec
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-10-26 20:48:05 -0300

    20:48

--- pages/contacto/index.js
+++ pages/contacto/index.js
@@ -26,7 +26,7 @@ const ContactForm = () => {
 
 	return (
       <>
-      <HeaderSub positon={top} titulo={lang=="ESP"?"CONTACTO":"CONTACT"} img="ofi4.jfif" />
+      <HeaderSub titulo={lang=="ESP"?"CONTACTO":"CONTACT"} img="ofi4.jfif" />
 		<div className={s.general_container}>
 
 			<div className={s.title_container}>
