c5ded62505dbdab0dfec50e2e6938ecb48584dc0
Jimancio <54536722+ignacioHourquescos@users.noreply.github.com>
2021-10-26 23:12:38 -0300

    23:12

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -38,9 +38,9 @@ const Header = () => {
                </iframe> */}
              <video autoPlay loop muted className={ss.video} 
 
->
-               <source src='./intro14.m4v' type='video/mp4' />
-            </video>
+
+             src='./intro14.m4v' type='video/mp4' />
+       
 
             {/* <YouTube videoId="deCFaF4TSOk?autoplay=1&mute=1&modestbranding=1&autohide=1&showinfo=0&controls=0" opts={opts}  />; */}
 
